4611cb73e458e7caade39688d06dedcd7d760235
Aina Mijoro <aina.mijoro@gmail.com>
2023-01-20 11:30:46 +0100

    test

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
 router.get('/year', (req, res) => {
   const date = new Date()
   let annee = date.getFullYear()
